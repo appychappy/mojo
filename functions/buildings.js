4a7be5b3c702d3ff2f76458be9ae28b75b208634
Matthew Phillips <mphill23@partner.jaguarlandrover.com>
2020-04-02 15:31:07 +0100

    returns correct buildings

--- functions/buildings.js
+++ functions/buildings.js
@@ -57,6 +57,6 @@ exports.handler = function(event, context, callback) {
   ];
   callback(null, {
     statusCode: 200,
-    body: "testing 123"
+    body: JSON.stringify(listOfBuildings)
   });
 };
